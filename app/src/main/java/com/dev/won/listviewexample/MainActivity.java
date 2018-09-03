ff5a236cb61c6837e38eb76ab6f4c5c97dd89e72
asag0704 <dhdbdnjs58@gmail.com>
2018-09-03 14:32:46 +0900

    rename repository

--- app/src/main/java/com/dev/won/listviewexample/MainActivity.java
+++ app/src/main/java/com/dev/won/listviewexample/MainActivity.java
@@ -2,12 +2,16 @@ package com.dev.won.listviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
     }
 }
